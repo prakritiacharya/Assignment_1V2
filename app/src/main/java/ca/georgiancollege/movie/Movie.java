ccc3ed52fba265edfc845dcc08891e7e9a3a592a
Prakriti Acharya <prakriti@acharya.com>
2025-07-24 19:04:36 -0400

    Added imdbID constructor for search result

--- app/src/main/java/ca/georgiancollege/movie/Movie.java
+++ app/src/main/java/ca/georgiancollege/movie/Movie.java
@@ -9,11 +9,13 @@ public class Movie {
     private String rating;
     private String plot;
 
-    public Movie(String title, String year, String posterUrl) {
+    public Movie(String title, String year, String posterUrl, String imdbID) {
         this.title = title;
         this.year = year;
         this.posterUrl = posterUrl;
+        this.imdbID = imdbID;
     }
+
     public String getTitle() { return title; }
     public String getYear() { return year; }
     public String getPosterUrl() { return posterUrl; }
