9ac0dd04def300c6f1c97a949a81cac05ec2a07b
Prakriti Acharya <prakriti@acharya.com>
2025-07-24 18:24:41 -0400

    Added click listener on the search button

--- app/src/main/java/ca/georgiancollege/movie/MainActivity.java
+++ app/src/main/java/ca/georgiancollege/movie/MainActivity.java
@@ -1,12 +1,15 @@
 package ca.georgiancollege.movie;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
 import ca.georgiancollege.movie.databinding.ActivityMainBinding;
@@ -23,5 +26,16 @@ public class MainActivity extends AppCompatActivity {
         adapter = new MovieAdapter();
         binding.recyclerView.setLayoutManager(new LinearLayoutManager(this));
         binding.recyclerView.setAdapter(adapter);
+
+        viewModel = new ViewModelProvider(this).get(MovieViewModel.class);
+
+        binding.searchButton.setOnClickListener(v -> {
+            String query = binding.movieInput.getText().toString();
+            if (TextUtils.isEmpty(query)) {
+                Toast.makeText(this, "Enter a movie name", Toast.LENGTH_SHORT).show();
+            } else {
+                viewModel.fetchMovies(query);
+            }
+        });
     }
 }
