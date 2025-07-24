5fd3c1957a5ccbf8e7f32956614db620d8b6d726
Prakriti Acharya <prakriti@acharya.com>
2025-07-24 18:12:47 -0400

    created MovieViewHolder class with text and image view

--- app/src/main/java/ca/georgiancollege/movie/MovieAdapter.java
+++ app/src/main/java/ca/georgiancollege/movie/MovieAdapter.java
@@ -1,36 +1,57 @@
-package ca.georgiancollege.movie;
+package ca.georgiancollege.movie_app;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHolder> {
     private List<Movie> movies = new ArrayList<>();
-}
 
-public void setMovies(List<Movie> movies) {
-    this.movies = movies;
-    notifyDataSetChanged();
-}
+    public void setMovies(List<Movie> movies) {
+        this.movies = movies;
+        notifyDataSetChanged();
+    }
 
-@NonNull
-@Override
-public MovieViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-    View view = LayoutInflater.from(parent.getContext())
-            .inflate(R.layout.item_movie, parent, false);
-    return new MovieViewHolder(view);
-}
+    @NonNull
+    @Override
+    public MovieViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_movie, parent, false);
+        return new MovieViewHolder(view);
+    }
 
-@Override
-public void onBindViewHolder(@NonNull MovieViewHolder holder, int position) {
-    Movie movie = movies.get(position);
-    holder.title.setText(movie.getTitle());
-    holder.year.setText(movie.getYear());
-    Picasso.get().load(movie.getPosterUrl()).into(holder.poster);
+    @Override
+    public void onBindViewHolder(@NonNull MovieViewHolder holder, int position) {
+        Movie movie = movies.get(position);
+        holder.title.setText(movie.getTitle());
+        holder.year.setText(movie.getYear());
+        Picasso.get().load(movie.getPosterUrl()).into(holder.poster);
+    }
+
+    @Override
+    public int getItemCount() {
+        return movies.size();
+    }
+
+    static class MovieViewHolder extends RecyclerView.ViewHolder {
+        TextView title, year;
+        ImageView poster;
+
+        public MovieViewHolder(@NonNull View itemView) {
+            super(itemView);
+            title = itemView.findViewById(R.id.titleText);
+            year = itemView.findViewById(R.id.yearText);
+            poster = itemView.findViewById(R.id.posterImage);
+        }
+    }
 }
