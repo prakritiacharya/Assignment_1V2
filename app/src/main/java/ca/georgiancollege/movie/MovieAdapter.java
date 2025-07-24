89f7e1e9beff22fce45f21188c32f1a4aa23560f
Prakriti Acharya <prakriti@acharya.com>
2025-07-24 18:08:27 -0400

    initialized the movie lists and imported classes and pakage

--- app/src/main/java/ca/georgiancollege/movie/MovieAdapter.java
+++ app/src/main/java/ca/georgiancollege/movie/MovieAdapter.java
@@ -1,4 +1,10 @@
 package ca.georgiancollege.movie;
 
-public class MovieAdapter {
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHolder> {
+    private List<Movie> movies = new ArrayList<>();
 }
