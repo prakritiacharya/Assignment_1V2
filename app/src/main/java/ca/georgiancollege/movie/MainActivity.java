5b37825d4b45890a7d09ae0e140c2e4ab74c50ac
Prakriti Acharya <prakriti@acharya.com>
2025-07-23 23:22:37 -0400

    Sync chnages and imported class

--- app/src/main/java/ca/georgiancollege/movie/MainActivity.java
+++ app/src/main/java/ca/georgiancollege/movie/MainActivity.java
@@ -8,6 +8,8 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import ca.georgiancollege.movie.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
