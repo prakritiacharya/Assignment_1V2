220d51b47b33f1d26f91fcc11ccdc18e71aad898
Prakriti Acharya <prakriti@acharya.com>
2025-07-24 17:56:58 -0400

    Modified the class

--- app/src/main/java/ca/georgiancollege/movie/Movie.java
+++ app/src/main/java/ca/georgiancollege/movie/Movie.java
@@ -1,4 +1,12 @@
 package ca.georgiancollege.movie;
 
 public class Movie {
-}
+    private final String title;
+    private final String year;
+    private final String posterUrl;
+
+    public Movie(String title, String year, String posterUrl) {
+        this.title = title;
+        this.year = year;
+        this.posterUrl = posterUrl;
+    }
